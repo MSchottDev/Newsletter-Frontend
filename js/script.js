1ab12f61120efd01e7474122359fec04bcfcda70
Matthias Schott <matthiasschott@hotmail.de>
2026-05-18 23:12:15 +0200

    feat: add button handling for resend email function fix: improve css style system fix: little style issues like missing  submit button:active

--- js/script.js
+++ js/script.js
@@ -1,49 +1,130 @@
+// Submittbutton immer auf Updates erhalten/subscribe schalten bei neuladen des dokuments und eintippen neuer email
+const emailInput = document.getElementById("email");
+const submitButton = document.getElementById("submitButton");
+
+emailInput.addEventListener("input", () => {
+    submitButton.innerText = "Updates erhalten";
+    submitButton.dataset.mode = "subscribe";
+});
+
 /* Hier wird HTML-Element newsletterForm gewählt, also Eingabefeld der EMail. Weiter setzt man den Eventlistener und
 über gibt das Event auf das reagiert wenrden soll, hier "submit". Dies wird vom Formular ausgelöst, wenn es versendet 
 werden soll. JS erstellt daraus ein Event, hier e benannt, in dem der Eventtyp und andere Parameter enthalten sind.
 Dieses Event e muss dann an eine funktion übergeben werden, in der definiert wird, was mit diesem Event passieren soll.*/ 
+document
+    .getElementById("newsletterForm")
+    .addEventListener("submit", async function (e) {
 
-document.getElementById("newsletterForm").addEventListener("submit", async function (e) {
     e.preventDefault();
 
     const email = e.target.email.value;
-    const messageElement = document.getElementById("message");
+    
 
-    const response = await fetch("https://localhost:7136/api/subscribers", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email })
-    });
+    const formCard = document.getElementById("form-card");
+    const statusCard = document.querySelector(".status-card");
 
-    const result = await response.json();
+    const formIcon = document.getElementById("form-message-icon");
+    const formText = document.getElementById("form-message-text");
 
-    if (response.status === 409) {
-        
-        document.getElementById("messageIcon").innerText = "✕";
-        document.getElementById("messageText").innerText = "Diese Email ist bereits registriert.";
-    }
-    else if (response.ok) {
-        
-        
+    const statusIcon = document.getElementById("icon");
+    const statusTitle = document.getElementById("title");
+    const statusMessage = document.getElementById("status-message");
+    const submitButton = document.getElementById("submitButton");
 
-        document.getElementById("newsletterForm").reset();
+    
+    // reset textmessage
+    formIcon.innerText = "";
+    formText.innerText = "";
+
+    
+
+    try {
+        // hier ert den Modus des Submitt-Buttons abfragen für subscribe oder resend
+        const mode = submitButton.dataset.mode;
+        let url = "https://localhost:7136/api/subscribers";
+        
+        if (mode === "resend") {
+            url += "/resend-confirmation";
+            }
+        const response = await fetch(
+            url,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({ email })
+            }
+        );
 
         
+        // email vorhanden (hier später einheitliche statuscodes)
         
-        // Wechsel Seitenstatus EINGABE auf ERFOLG
-        document.getElementById("form-state").style.display = "none";
-        document.getElementById("success-state-index").style.display = "block";
-    }
-    else {
+        if (response.status === 409) {
+
+            formIcon.innerText = "✕";
+            formIcon.className = "message-icon status-error";
+            // hier noch ausgabe anpassen: message header + message text
+            formText.innerText =
+                                "Diese E-Mail ist bereits vorhanden. " +
+                                "Klicken Sie auf \"Bestätigen\" in Ihrer Bestätigungsmail, " +
+                                "um die Registrierung abzuschließen. " +
+                                "Wenn Sie eine neue Bestätigungsmail benötigen, " +
+                                "klicken Sie einfach auf \"Neu senden\":";
+            
+            submitButton.innerText = "Neu senden";
+            submitButton.dataset.mode = "resend";
+
+            return;
+        }
+
         
-        messageElement.innerText = "Ein Fehler ist aufgetreten.";
-        messageElement.style.color = "red";
+        // erfolg (einheitliche statuscodes !)
+        
+        if (response.ok) {
+
+            e.target.reset();
+
+            formCard.style.display = "none";
+            //statusCard.style.display = "flex";
+            statusCard.classList.remove("is-hidden");
+            formCard.classList.add("is-hidden");
+
+            statusIcon.innerText = "";
+            statusIcon.className =
+                "status-icon status-neutral";
+
+            statusTitle.innerText = "E-Mail gespeichert";
+
+            statusMessage.innerText =
+                "Sie erhalten eine Email mit dem Beestätigungs-Link.Bitte schauen Sie auch in Ihrem Spam-Ordner nach.";
+
+            return;
+        }
+
+        
+        // error
+        
+        formIcon.innerText = "!";
+        formIcon.className = "message-icon status-error";
+
+        formText.innerText =
+            "Ein Fehler ist aufgetreten.";
+
+    } catch (error) {
+
+        formIcon.innerText = "!";
+        formIcon.className = "message-icon status-error";
+
+        formText.innerText =
+            "Server nicht erreichbar.";
     }
 });
 
-// close-Button
+
+
+// close button
+
 document
     .getElementById("closeButton")
     .addEventListener("click", () => {
--- js/status.js
+++ js/status.js
@@ -3,60 +3,127 @@ const state = params.get("state");
 
 const icon = document.getElementById("icon");
 const title = document.getElementById("title");
-const message = document.getElementById("message");
+const message = document.getElementById("status-message");
 const githubIcon = document.getElementById("githubIcon");
 
-switch (state) {
 
-    case "confirmed":
-        icon.innerText = "✓";
-        icon.className = "status-icon status-success";
-        githubIcon.style.filter = "brightness(0) saturate(100%) invert(64%) sepia(62%) saturate(558%) hue-rotate(93deg) brightness(91%) contrast(92%)";
-        title.innerText = "Anmeldung erfolgreich";
-        message.innerText =
-            "Danke für die Anmeldung. Sie erhalten ab sofort Updates zu meinen GitHub-Projekten.";
-        break;
+// icons anpassen
 
-    case "already-confirmed":
-        icon.innerText = "✓";
-        icon.className = "status-icon status-warning";
-        githubIcon.style.filter = "brightness(0) saturate(100%) invert(73%) sepia(78%) saturate(773%) hue-rotate(347deg) brightness(99%) contrast(95%)";
-        title.innerText = "Bereits bestätigt";
-        message.innerText =
-            "Diese Email-Adresse wurde bereits bestätigt.";
-        break;
+const successFilter =
+    "brightness(0) saturate(100%) invert(64%) sepia(62%) saturate(558%) hue-rotate(93deg) brightness(91%) contrast(92%)";
 
-    case "unsubscribed":
-        icon.innerText = "✓";
-        icon.className = "status-icon status-success";
-        githubIcon.style.filter = "brightness(0) saturate(100%) invert(64%) sepia(62%) saturate(558%) hue-rotate(93deg) brightness(91%) contrast(92%)";
-        title.innerText = "Abmeldung erfolgreich";
-        message.innerText =
-            "Sie wurden erfolgreich vom Newsletter abgemeldet.";
-        break;
+const warningFilter =
+    "brightness(0) saturate(100%) invert(73%) sepia(78%) saturate(773%) hue-rotate(347deg) brightness(99%) contrast(95%)";
 
-    case "error":
-        icon.innerText = "✕";
-        icon.className = "status-icon status-error";
-        githubIcon.style.filter = "brightness(0) saturate(100%) invert(43%) sepia(95%) saturate(1426%) hue-rotate(332deg) brightness(95%) contrast(95%)";
-        title.innerText = "Fehler";
-        message.innerText =
-            "Der Link ist ungültig oder abgelaufen.";
-        break;
+const errorFilter =
+    "brightness(0) saturate(100%) invert(43%) sepia(95%) saturate(1426%) hue-rotate(332deg) brightness(95%) contrast(95%)";
 
-    default:
-        icon.innerText = "?";
-        icon.className = "status-icon status-warning";
 
-        title.innerText = "Status";
-        message.innerText = "Unbekannter Status.";
-        break;
+
+// status handling
+
+if (icon && title && message && githubIcon) {
+
+    switch (state) {
+
+        
+        // angemeldet
+        
+        case "confirmed":
+
+            icon.innerText = "✓";
+            icon.className = "status-icon status-success";
+
+            githubIcon.style.filter = successFilter;
+
+            title.innerText = "Anmeldung erfolgreich";
+
+            message.innerText =
+                "Danke für die Anmeldung. Sie erhalten ab sofort Updates zu meinen GitHub-Projekten.";
+
+            break;
+
+
+        
+        // bereits angemeldt
+        
+        case "already-confirmed":
+
+            icon.innerText = "✓";
+            icon.className = "status-icon status-warning";
+
+            githubIcon.style.filter = warningFilter;
+
+            title.innerText = "Bereits bestätigt";
+
+            message.innerText =
+                "Diese E-Mail-Adresse wurde bereits bestätigt.";
+
+            break;
+
+
+        
+        // erfolg abgemeldet
+        
+        case "unsubscribed":
+
+            icon.innerText = "✓";
+            icon.className = "status-icon status-success";
+
+            githubIcon.style.filter = successFilter;
+
+            title.innerText = "Abmeldung erfolgreich";
+
+            message.innerText =
+                "Sie wurden erfolgreich vom Newsletter abgemeldet.";
+
+            break;
+
+
+        
+        // error
+        
+        case "error":
+
+            icon.innerText = "✕";
+            icon.className = "status-icon status-error";
+
+            githubIcon.style.filter = errorFilter;
+
+            title.innerText = "Fehler";
+
+            message.innerText =
+                "Der Link ist ungültig oder abgelaufen.";
+
+            break;
+
+
+        
+        // sonstiges
+        
+        default:
+
+            icon.innerText = "?";
+            icon.className = "status-icon status-warning";
+
+            githubIcon.style.filter = warningFilter;
+
+            title.innerText = "Status";
+
+            message.innerText =
+                "Unbekannter Status.";
+
+            break;
+    }
 }
 
-// close-Button
+
+
+// close
+
 document
     .getElementById("closeButton")
-    .addEventListener("click", () => {
+    ?.addEventListener("click", () => {
 
         window.close();
 });
